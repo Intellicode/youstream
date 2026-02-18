847590d53fa5deed30c5da4d4b0119a0fd10a66c
Tom Hastjarjanto <tom@mailx.nl>
2026-02-18 21:38:19 +0100

    chore: formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # youstream
+
 youstream
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,6 +1,12 @@
 export async function cors(c: any, next: Function) {
   await next();
   c.res.headers.set("Access-Control-Allow-Origin", "*");
-  c.res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  c.res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  c.res.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  c.res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization",
+  );
 }
